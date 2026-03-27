011afcbee6517984bf973678a77b001335e8e57b
rqh656418510 <656418510@qq.com>
2026-03-27 19:05:09 +0800

    Refactor tipue search database generator

--- index.js
+++ index.js
@@ -3,61 +3,76 @@ var util = require('hexo-util');
 var config = hexo.config;
 
 // default file path
-var path =  "/tipuesearch/tipuesearch_content.js";
+var path = "/tipuesearch/tipuesearch_content.js";
 
 // custom file path
-if(config.tipue_search_db && config.tipue_search_db.path){
-  path = config.tipue_search_db.path;
+if (config.tipue_search_db && config.tipue_search_db.path) {
+    path = config.tipue_search_db.path;
 }
 
 hexo.extend.generator.register('generator-tipue-search-db', hexo_generator_tipue_search_db);
 
 function hexo_generator_tipue_search_db(site) {
     var minify = function (str) {
-            return util.stripHTML(str).trim().replace(/\n/g, ' ').replace(/\s+/g, ' ');
-        },
+        if (!str) return '';
 
-        keys = {
-            title: true,
-            url: true,
-            text: true,
-            tags: true
-        },
+        return util.stripHTML(str)
+            .trim()
+            .replace(/\n/g, ' ')
+            .replace(/\s+/g, ' ');
+    };
 
-        json = {};
+    var expandKeywords = function (str) {
+        if (!str) return '';
+
+        return str
+            .replace(/c\+\+/gi, 'C++ Cplusplus')
+            .replace(/c#/gi, 'C# Csharp');
+    };
+
+    var keys = {
+        title: true,
+        url: true,
+        text: true,
+        tags: true
+    };
+
+    var json = {};
 
     var catags = function (item) {
-            return item.name.replace(/\s+/g, '-').toLowerCase();
-        },
+        return expandKeywords(item.name)
+            .replace(/\s+/g, '-')
+            .toLowerCase();
+    };
 
-        postsContent = site.posts.sort('-date').filter(function (post) {
-            return post.published;
-        }).map(function (post) {
-            var actualPost = {};
+    var postsContent = site.posts.sort('-date').filter(function (post) {
+        return post.published;
+    }).map(function (post) {
+        var actualPost = {};
 
-            Object.getOwnPropertyNames(keys).forEach(function (item) {
-                switch (item) {
-                    case 'text':
-                        return actualPost[item] = minify(post.content);
+        Object.getOwnPropertyNames(keys).forEach(function (item) {
+            switch (item) {
+                case 'text':
+                    return actualPost[item] = expandKeywords(minify(post.content));
 
-                    case 'tags':
-                        return actualPost[item] = post.tags.map(catags).join(' ');
+                case 'tags':
+                    return actualPost[item] = post.tags.map(catags).join(' ');
 
-                    case 'url':
-                        return actualPost[item] = hexo.config.root + post['path'];
+                case 'url':
+                    return actualPost[item] = hexo.config.root + post['path'];
 
-                    default:
-                        return actualPost[item] = post[item];
-                }
-            });
+                default:
+                    return actualPost[item] = post[item];
+            }
+        });
 
-            return actualPost;
-        }),
+        return actualPost;
+    });
 
-    pagesContent = site.pages.sort('-date').map(function (page) {
+    var pagesContent = site.pages.sort('-date').map(function (page) {
         var actualPage = {};
 
-        if(hexo.config.tipue_search_db && hexo.config.tipue_search_db.exclude_page) {
+        if (hexo.config.tipue_search_db && hexo.config.tipue_search_db.exclude_page) {
             actualPage.url = "";
             actualPage.text = "";
             actualPage.tags = "";
@@ -68,7 +83,7 @@ function hexo_generator_tipue_search_db(site) {
         Object.getOwnPropertyNames(keys).forEach(function (item) {
             switch (item) {
                 case 'text':
-                    return actualPage[item] = minify(page.content);
+                    return actualPage[item] = expandKeywords(minify(page.content));
 
                 case 'url':
                     return actualPage[item] = hexo.config.root + page['path'];
@@ -82,7 +97,7 @@ function hexo_generator_tipue_search_db(site) {
     });
 
     json.pages = postsContent.concat(pagesContent);
-    dbContent = "var tipuesearch = " + JSON.stringify(json);
+    var dbContent = "var tipuesearch = " + JSON.stringify(json);
 
     return {
         path: path,
